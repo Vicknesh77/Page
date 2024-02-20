568ce0a6248a4bb1383a0f65ba15c4cebfacc399
Vicknesh77 <vickneshi56@gmail.com>
2024-02-20 13:27:20 +0530

    ok

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
             <input className=' rounded-md mx-1 my-5 py-1 px-8 md:px-12 text-black' type="email" /><br />
             <label htmlFor="">Industry</label><br />
             <input className=' rounded-md mx-1 my-5 py-1 px-8 md:px-12 text-black' type="text" /><br />
-            <button className='border rounded-lg mt-5 items-center bg-emerald-500 font-bold tex-2xl border-spacing-3  py-2 px-[80px]  '>Claim Now</button>
+            <button className='border rounded-lg mt-5 items-center bg-emerald-500 font-bold tex-2xl border-spacing-3  py-2 px-[80px] md:px-[100px]  '>Claim Now</button>
 
         </div>
       
